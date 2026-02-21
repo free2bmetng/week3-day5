3b0b295668cf6c7b09e0cb8f3129e30c3bedf504
Tiarra G <free2bmetng@gmail.com>
2026-02-20 20:06:58 -0600

    for each tags

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ const scores = [10, 25, 18, 30];
 let totalScore = 0;
 
 for (let i = 0; i < scores.length; i++) {
-    totalScore += scores[i];
-   }
+  totalScore += scores[i];
+}
 
 console.log(totalScore);
+
+tags.forEach(tag);
+function tag(words) {
+  console.log(words + " 2026");
+}
 
 const doubledScores = scores.map((score) => {
   return score * 2;
 });
+console.log(scores);
 
 console.log(doubledScores);
